cd81978ddfd6c586148cf3f0e128129fc3051189
blackadress <blackadress@github.com>
2019-09-28 11:55:28 -0500

    middleware

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
+import bodyParser from 'body-parser'
 import express from 'express'
 
 const setup = () => {
   let app = express()
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: true }))
 
   return app
 }
