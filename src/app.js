92e58c4219d4ba86a2d684b26400eec0d6805c8b
blackadress <blackadress@github.com>
2019-09-28 12:04:04 -0500

    static files + pug

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 import bodyParser from 'body-parser'
 import express from 'express'
 
+import path from 'path'
+
 const setup = () => {
   let app = express()
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({ extended: true }))
+
+  app.set('views', path.join(__dirname, 'views'))
+  app.set('view engine', 'pug')
+  
+  app.use('static', express.static(path.join(__dirname, 'static')))
+  app.set('public', express.static(path.join(__dirname, 'public')))
 
   return app
 }
