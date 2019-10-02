cc06d174d9e65ac9338cc7fa9d861c0922c7e333
blackadress <blackadress@github.com>
2019-10-01 20:35:14 -0500

    correcciones

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import bodyParser from 'body-parser'
 import express from 'express'
 
+import { routes } from './routes'
+
 const setup = () => {
   let app = express()
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({ extended: true }))
+
+
+  routes.map(route => route(app))
 
   return app
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import { setup } from './app'
-import { routes } from './routes'
 
 let app = setup()
-
-routes.map(route => route(app))
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
